fc5b4968249c7fa84a5906031a073a01585a8a91
Jake Billo <jake@jakebillo.com>
2017-03-12 22:36:12 -0400

    Fix volumes - now point to correct places on FS

--- bringup.sh
+++ bringup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 docker run --name ldap-service --hostname ldap-service --detach \
-  --volume /data/slapd/database:/opt/slapd/database \
-  --volume /data/slapd/config:/opt/slapd/config \
+  --volume /opt/slapd/database:/data/slapd/database \
+  --volume /opt/slapd/config:/data/slapd/config \
   osixia/openldap:1.1.8 || true
 docker run --name phpldapadmin-service --hostname phpldapadmin-service --link ldap-service:ldap-host --env PHPLDAPADMIN_LDAP_HOSTS=ldap-host --detach osixia/phpldapadmin:0.6.12 || true
 
@@ -12,5 +12,5 @@ echo "Login DN: cn=admin,dc=example,dc=org"
 echo "Password: admin"
 
 docker run --name sftpthethings --hostname sftpthethings --link ldap-service:ldap-host -p 44022:22 --detach \
-	--volume /sftproot:/opt/sftproot \
+	--volume /opt/sftproot:/sftproot \
 	jbillo/sftpthethings:latest
